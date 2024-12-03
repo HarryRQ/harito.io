496858e352260622e42f8781cca479762a6c282c
Harito <harito@Haritos-MacBook-Pro.local>
2024-12-03 00:26:36 -0400

    Added array that contains info about my experiences.

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const EXPERIENCE = [
+  {
+    title: "Associate Software Engineer",
+    company: "Lockheed Martin",
+    location: "Aguadilla, P.R.",
+    duration: "June 2023 – Present",
+    description: [
+      "Enhancing object-oriented design to modernize the F-16’s Modular Mission Computer (MMC) by converting legacy Ada code to C++, ensuring accurate and effective code migration.",
+      " Redesigned and implemented a critical module responsible for reporting subsystem failures, enhancing functionality and efficiency using object-oriented principles.",
+      " Led the development of a system to efficiently monitor and process communication statuses across multiple subsystems, ensuring efficient operation under high data volumes.",
+      " Appointed to review and provide feedback on merge requests from new hires, mentoring them on coding standards, best practices, and guiding them through team workflows to ensure a successful onboarding process.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company: "Climatology Office – University of Puerto Rico – Mayagüez Campus",
+    location: "Mayagüez, P.R.",
+    duration: "April 2022 – August 2022",
+    description: [
+      "Developed a user-friendly climatology web application using React and Typescript, enhancing accessibility and user experience.",
+      " Extracted and dynamically integrated climate data from NOAA and USGS official sources into the web application, ensuring up-to-date information.",
+      " Revamped front-end development to resolve issues with data display logic, ensuring accurate presentation of climate data from stations across Puerto Rico.",
+    ],
+  },
+  {
+    title: "Computer Science Intern",
+    company: "National Weather Service",
+    location: "San Juan, P.R.",
+    duration: "June 2022 – July 2022",
+    description: [
+      "Managed and analyzed large datasets from the National Oceanic and Atmospheric Administration (NOAA) to classify and rank weather hazards for each municipality in Puerto Rico, providing critical information for disaster preparedness efforts.",
+      " Utilized QGIS to create comprehensive threat and vulnerability maps for the Storm Ready program, enabling San Juan forecasters to seamlessly integrate them into their warning operations.",
+      " Developed a geo-referenced database for end-users and presented threat and vulnerability map results to the Mayagüez community in Puerto Rico and the University of the Incarnate Word of Texas, emphasizing the importance of geo-referenced data in disaster preparation and response.",
+    ],
+  },
+  {
+    title: "Undergraduate Researcher",
+    company: "University of Puerto Rico – Mayagüez Campus",
+    location: "Mayagüez, P.R.",
+    duration: "January 2022 – May 2022",
+    description: [
+      "Investigated the application of evolutionary algorithms, including genetic algorithms, to optimize solutions for predicting eye pigmentation based on genetic markers and evolutionary principles.",
+      " Proposed a methodology integrating genetic insights from OCA2 and HERC2 genes with evolutionary algorithms to enhance predictive models for newborn eye pigmentation.",
+    ],
+  },
+];
